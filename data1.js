89c342f31c6c3f5d620fc9fd255ddb6685f6efcb
ArpitaAgrawal <arpitaagrawal9109@gmail.com>
2026-02-23 17:17:36 +0530

    to add new text

--- data1.js
+++ data1.js
@@ -1,4 +1,5 @@
-const holi="Holi se ek din pehle Holika Dahan hota hai"
-const holi1="Is din log raat me aag jalate hain jo burai par achchai ki jeet ka pratik hai"
-const holi2="Ye kahani Prahlad aur Holika se judi hui hai"
-const holi3="3Ye kahani Prahlad aur Holika se judi hui haiPrahlad aur Holika se judi hui hai"
+`constHoli se ek din pehle Holika Dahan hota hai
+Is din log raat me aag jalate hain jo burai par achchai ki jeet ka pratik hai
+Ye kahani Prahlad aur Holika se judi hui hai`
+`hello manu`
+`hello manu`
